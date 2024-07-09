0727bb6752d0003ccec3d5717c048dabb3e10184
PhamHoaiTan <95225335+PhamHoaiTan@users.noreply.github.com>
2024-07-09 16:24:06 +0700

    commit img footer

--- web-game/src/assets/assets.js
+++ web-game/src/assets/assets.js
@@ -1,7 +1,8 @@
 import logo from './Website logo.webp'
 import account from './account.png'
-
+import facebook from './facebook.jpg'
 export const assets = {
     logo,
-    account
+    account,
+    facebook
 }
--- web-game/src/components/Footer/Footer.jsx
+++ web-game/src/components/Footer/Footer.jsx
@@ -1,13 +1,15 @@
 // src/components/Footer/Footer.jsx
 import React from 'react';
 import './Footer.css';
+import { assets } from '../../assets/assets';
 
 function Footer() {
   return (
     <footer className="Footer">
       <p>FGZ COMPANY, 2024, ALL RIGHT RESERVED</p>
+      <hr></hr>
       <nav>
-        <a href="#">Facebook</a>
+        <a href="#"><img src={assets.facebook} alt="" /></a>
         <a href="#">Instagram</a>
         <a href="#">TikTok</a>
         <a href="#">YouTube</a>
