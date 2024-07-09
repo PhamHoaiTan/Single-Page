cdcf33aa0dc8fd43d492552fc340150d7ff1c668
PhamHoaiTan <95225335+PhamHoaiTan@users.noreply.github.com>
2024-07-09 17:10:32 +0700

    summit code

--- web-game/src/assets/assets.js
+++ web-game/src/assets/assets.js
@@ -1,8 +1,16 @@
 import logo from './Website logo.webp'
 import account from './account.png'
-import facebook from './facebook.jpg'
+import facebook from './facebook.png'
+import ins from './ins.png'
+import telegram from './telegram.png'
+import tiktok from './tiktok.png'
+import youtube from './youtube.png'
 export const assets = {
     logo,
     account,
-    facebook
+    facebook,
+    ins,
+    telegram,
+    tiktok,
+    youtube
 }
--- web-game/src/components/Footer/Footer.jsx
+++ web-game/src/components/Footer/Footer.jsx
@@ -6,16 +6,36 @@ import { assets } from '../../assets/assets';
 function Footer() {
   return (
     <footer className="Footer">
-      <p>FGZ COMPANY, 2024, ALL RIGHT RESERVED</p>
-      <hr></hr>
-      <nav>
-        <a href="#"><img src={assets.facebook} alt="" /></a>
-        <a href="#">Instagram</a>
-        <a href="#">TikTok</a>
-        <a href="#">YouTube</a>
-        <a href="#">Telegram</a>
-        <a href="#">LinkedIn</a>
+      <h3>Connect With Us:</h3>
+      <nav className='item-social'>
+        <a href="#"><img src={assets.facebook} alt=""  /></a>
+        <a href="#"><img src={assets.ins} alt="" /></a>
+        <a href="#"><img src={assets.tiktok} alt="" /></a>
+        <a href="#"><img src={assets.youtube} alt="" /></a>
+        <a href="#"><img src={assets.telegram} alt="" /></a>
       </nav>
+      <hr />
+    <div className="footer-review">
+      <div className="about-us">
+        <h4>About Us:</h4>
+        <p>Our website specializes in offering the latest and most exciting games for passionate gamers. With a wide variety of genres and platforms, we are committed to providing the best gaming experience for everyone.</p>
+      </div>
+      <div className="contact-us">
+        <h4>Contact Us:</h4>
+        <p>Email: support@gamestore.com <br />
+          Phone: 123-456-789 <br />
+          Address: 123 ABC Street, XYZ District, Ho Chi Minh City</p>
+      </div>
+      <div className="customer-sp">
+        <h4>Customer Support</h4>
+        <p>
+          Frequently Asked Questions (FAQ) <br />
+          Return Policy <br />
+          Privacy Policy  <br />
+          Terms of Service  <br />
+        </p>
+      </div>
+    </div>
     </footer>
   );
 }
