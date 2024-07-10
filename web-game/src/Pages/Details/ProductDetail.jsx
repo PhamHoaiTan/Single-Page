a1e99859441362fcdec17abca45d5961414caaf1
PhamHoaiTan <95225335+PhamHoaiTan@users.noreply.github.com>
2024-07-10 12:36:21 +0700

    update css Product

--- web-game/src/Pages/Details/ProductDetail.jsx
+++ web-game/src/Pages/Details/ProductDetail.jsx
@@ -15,20 +15,30 @@ function ProductDetail() {
 
   return (
     <div className="ProductDetail">
-      <div className="name">
-      <h1>{product.name}</h1>
-      </div> 
+      <div className="name"><h1 className='deltail-h1'>{product.name}</h1></div> 
       <div className="details-content">
-
+        {/* Detail Left */}
+        <div className="detail-left">
       <img src={product.image} alt={product.name} />
-      <p><strong>Price:</strong> {product.price}</p>
-      <p><strong>Genre:</strong> {product.genre}</p>
+      <div className="detail-description"><p>{product.description}</p></div>
+      <p><span className='genres'>Genres:</span><br /> {product.genre}</p>
+        </div>
+        {/* Detail Right */}
+      <div className="detail-right">
+      <div  className='base-game'><p>Base Game</p></div>
+      <p>{product.price}</p>
+      <div className="detail-bt">
+      <button className='buyNow'>BUY NOW</button><br></br>
+      <button className='addToCard'>ADD TO CARD</button>
+      </div>
       <p><strong>Release Date:</strong> {product.releaseDate}</p>
       <p><strong>Developer:</strong> {product.developer}</p>
       <p><strong>Publisher:</strong> {product.publisher}</p>
-      <p><strong>Description:</strong> {product.description}</p>
       </div>
+      </div>
+      <div className="suggestion">
       <Suggestions genre={product.genre} currentGameId={product.id} />
+      </div>
     </div>
   );
 }
