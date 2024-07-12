54c6894297a49b7472b5e1953891fd4bc457634c
PhamHoaiTan <95225335+PhamHoaiTan@users.noreply.github.com>
2024-07-12 15:45:16 +0700

    commit login

---
+++ web-game/src/components/LoginPopup/LoginPopup.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { assets } from '../../assets/assets'
+import './LoginPopup.css'
+const LoginPopup = ({setShowLogin}) => {
+
+    const [currState,setCurrState] = useState("Sign Up")
+
+  return (
+    <div className='login-popup'>
+        <form className='login-popup-container'>
+            <div className="login-popup-tilte">
+                <h2>{currState}</h2>    
+                <img src={assets.cross_icon} alt="" onClick={()=>setShowLogin(false)}/>
+                </div>
+            <div className="login-popup-inputs">
+                {currState==="Sign In"?<></>:<input type="text" placeholder='Your Name' required />}
+                <input type="text" placeholder='Your email' required />
+                <input type="password" placeholder='Password' required />
+            </div>
+            <button>{currState==="Sign Up"?"Create account":"Login"}</button>
+            {currState==="Sign Up"?  <div className="login-popup-condition">
+                <input type="checkbox" required />
+                <p>By continuing, i agree to the terms of use & privacy policy</p>
+            </div>:<></>}
+            {currState==="Sign Up"?<p>Create a new account ?<span onClick={()=>setCurrState("Sign In")}> Click here</span></p>
+            :<p>Already have an account? <span onClick={()=>setCurrState("Sign Up")}> Login here</span></p>
+            }
+        </form>    
+    </div>
+  )
+}
+
+export default LoginPopup
